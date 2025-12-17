9a6e83a3f2349decf6cdb45c20c0dabfbdc3634c
Muhammad Jawad Iqbal <jawadiqbal962@gmail.com>
2025-12-17 13:59:05 +0500

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: ["/api/", "/admin/"],
     },
-    sitemap: "https://organicbazaar.com/sitemap.xml",
+    sitemap: "https://organicbazaar.online/sitemap.xml",
   }
 }
